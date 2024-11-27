017d08f4c0ac01ee9daaf978fcd3719bc6029888
gradiel9901 <167274375+gradiel9901@users.noreply.github.com>
2024-11-27 18:42:04 +0800

    BAUTISTA_HALABI_SANTIAGO_FINALS

---
+++ Assets/Scripts/KitchenGameManager.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenGameManager : MonoBehaviour
+{
+    public static KitchenGameManager Instance { get; private set; }
+
+    public event EventHandler OnStateChanged;
+    public event EventHandler OnGamePaused;
+    public event EventHandler OnGameUnpaused;
+
+    private enum State
+    {
+        WaitingToStart,
+        CountdownToStart,
+        GamePlaying,
+        GameOver,
+    }
+
+    private State state;
+    private float countdownToStartTimer = 3f;
+    private float gamePlayingTimer;
+    private float elapsedGameTime; // To track total elapsed time
+    [SerializeField] private float gamePlayingTimerMax = 10f;
+    private bool isGamePaused = false;
+
+    private void Awake()
+    {
+        Instance = this;
+        state = State.WaitingToStart;
+    }
+
+    private void Start()
+    {
+        GameInput.Instance.OnPauseAction += GameInput_OnPauseAction;
+        GameInput.Instance.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, EventArgs e)
+    {
+        if (state == State.WaitingToStart)
+        {
+            state = State.CountdownToStart;
+            OnStateChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+
+    public void DeductTimeOnDishSubmit(float timeToDeduct)
+    {
+        if (state == State.GamePlaying)
+        {
+            gamePlayingTimer -= timeToDeduct;
+            // Ensure the timer doesn't go below zero
+            gamePlayingTimer = Mathf.Max(gamePlayingTimer, 0f);
+        }
+    }
+
+    private void GameInput_OnPauseAction(object sender, EventArgs e)
+    {
+        TogglePauseGame();
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.WaitingToStart:
+                break;
+            case State.CountdownToStart:
+                countdownToStartTimer -= Time.deltaTime;
+                if (countdownToStartTimer < 0f)
+                {
+                    state = State.GamePlaying;
+                    gamePlayingTimer = gamePlayingTimerMax;
+                    elapsedGameTime = 0f; // Reset elapsed time when the game starts
+                    OnStateChanged?.Invoke(this, EventArgs.Empty);
+                }
+                break;
+            case State.GamePlaying:
+                gamePlayingTimer -= Time.deltaTime;
+                elapsedGameTime += Time.deltaTime; // Increment elapsed time
+                if (gamePlayingTimer < 0f)
+                {
+                    state = State.GameOver;
+                    OnStateChanged?.Invoke(this, EventArgs.Empty);
+                }
+                break;
+            case State.GameOver:
+                break;
+        }
+    }
+
+    public bool IsGamePlaying()
+    {
+        return state == State.GamePlaying;
+    }
+
+    public bool IsCountdownToStartActive()
+    {
+        return state == State.CountdownToStart;
+    }
+
+    public float GetCountdownToStartTimer()
+    {
+        return countdownToStartTimer;
+    }
+
+    public bool IsGameOver()
+    {
+        return state == State.GameOver;
+    }
+
+    public float GetGamePlayingTimerNormalized()
+    {
+        return 1 - (gamePlayingTimer / gamePlayingTimerMax);
+    }
+
+    public void TogglePauseGame()
+    {
+        isGamePaused = !isGamePaused;
+        if (isGamePaused)
+        {
+            Time.timeScale = 0f;
+            OnGamePaused?.Invoke(this, EventArgs.Empty);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            OnGameUnpaused?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public void AddTimeOnDishSubmit(float timeToAdd)
+    {
+        if (state == State.GamePlaying)
+        {
+            gamePlayingTimer += timeToAdd;
+            // Ensure the timer does not exceed the max allowed time
+            gamePlayingTimer = Mathf.Min(gamePlayingTimer, gamePlayingTimerMax);
+        }
+    }
+
+    public float GetElapsedTime()
+    {
+        return elapsedGameTime; // Return the total elapsed time
+    }
+}
